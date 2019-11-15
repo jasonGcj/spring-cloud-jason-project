f63fb035fca6c55a8c46db0d77e793697e2cd918
GCJ <zhuweixing001@deppon.com>
2019-11-15 14:08:18 +0800

    更换云服务的mysql地址

--- spring-cloud-user/src/main/java/com/jason/user/interceptor/JwtInterceptor.java
+++ spring-cloud-user/src/main/java/com/jason/user/interceptor/JwtInterceptor.java
@@ -2,6 +2,8 @@ package com.jason.user.interceptor;
 
 import com.jason.utils.JwtUtil;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.support.RequestContext;
@@ -18,12 +20,14 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("经过了拦截器");
         String header = request.getHeader("token");
         String s = JwtUtil.checkToken(header);
         if(StringUtils.isBlank(s)){
+            LOGGER.info("用户为登录不能访问");
             return false;
         }
         System.out.println("放行");
